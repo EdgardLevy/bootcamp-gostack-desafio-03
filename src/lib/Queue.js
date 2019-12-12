e373cc197d288df5635ad725a1d1fb559604305f
Edgard Levy <edgardlevy.apple@gmail.com>
2019-12-12 18:00:25 -0200

    - adicionado lib concurrently e um novo script pra rodar o banco e o modulo da fila

--- src/lib/Queue.js
+++ src/lib/Queue.js
@@ -16,6 +16,7 @@ class Queue {
    * Start jobs queues
    */
   init() {
+    console.log('Queue init');
     jobs.forEach(({ key, handle }) => {
       this.queues[key] = {
         bee: new Bee(key, { redis: redisConfig }),
@@ -25,10 +26,12 @@ class Queue {
   }
 
   add(queue, job) {
+    console.log('Queue add');
     return this.queues[queue].bee.createJob(job).save();
   }
 
   processQueue() {
+    console.log('Queue processQueue');
     jobs.forEach(job => {
       const { bee, handle } = this.queues[job.key];
       bee.on('failed', this.handleFailure).process(handle);
